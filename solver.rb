b17953259b8fc17cafeeb4478935e487086dd350
BELINDA <kennyegun240@gmail.com>
2023-01-12 20:29:59 +0100

    Add codes to solve reverse and fizzbuzz challenges

--- solver.rb
+++ solver.rb
@@ -6,10 +6,30 @@ class Solver
 
     val = num
     counter = 1
-    while counter < num 
+    while counter < num
       val *= counter
       counter += 1
     end
     val
+  end
+
+  def reverse(string)
+    raise Exception, 'use strings only' if string.class != String
+
+    string.reverse!
+  end
+
+  def fizzbuzz(num)
+    raise Exception, 'use only positive integers' if num.instance_of?(String)
+
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
   end
 end
